20cc363e9a382b1f627a7f78c5cd0d5e79e840b0
ascended27 <ascended27@gmail.com>
2017-11-14 21:25:55 -0500

    Doc routes
    Started api-doc routes

---
+++ server/server.js
@@ -0,0 +1,30 @@
+var express = require('express'),
+	logger = require('morgan'),
+	app = express(),
+	template = require('jade').compileFile(__dirname+'/src/docs/api-docs/templates/homepage.jade');
+
+const path = require('path');
+
+app.use(logger('dev'));
+
+app.use(express.static(path.join(__dirname+"/src/docs/static")));
+
+app.get('/docs/api', function (req,res){
+	try{
+		console.log(path.join(__dirname+'/src/docs/api-docs'));
+		var html = template({title:'Home'});
+		res.send(html);
+	} catch(e){
+		next(e);
+	}
+});
+
+
+
+app.get('*',function (req,res){
+    res.redirect("/docs/api");
+});
+
+var server = app.listen(8090,function(){
+	console.log("Server listening on port 8090");
+});
