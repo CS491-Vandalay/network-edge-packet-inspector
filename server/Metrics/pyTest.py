82397b3b50eaa9e21fee4706fbc768729e0932b2
ascended27 <ascended27@gmail.com>
2018-01-25 18:01:36 -0500

    Added some routes for generating metrics.

---
+++ server/Metrics/pyTest.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def main():
+    data = {}
+    for i in range(1,200):
+        data["".join(["arg",str(i)])] = str(i)
+
+    print data , "\n"
+
+if __name__ == '__main__':
+    main()
--- server/server.js
+++ server/server.js
@@ -1,30 +1,145 @@
+/***************************************************
+ *              SERVER STUFF
+ **************************************************/
+
 var express = require('express'),
-	logger = require('morgan'),
-	app = express(),
-	template = require('pug').compileFile(__dirname+'/src/docs/api-docs/templates/base.pug');
+    spawn = require('child_process').spawn,
+    config = require('config'),
+    logger = require('morgan'),
+    app = express(),
+    template = require('pug').compileFile(__dirname + '/src/docs/api-docs/templates/base.pug');
 
 const path = require('path');
 
 app.use(logger('dev'));
 
-app.use(express.static(path.join(__dirname+"/src/docs/static")));
-
-app.get('/docs/api', function (req,res){
-	try{
-		console.log(path.join(__dirname+'/src/docs/api-docs'));
-		var html = template({title:'Home'});
-		res.send(html);
-	} catch(e){
-		console.log(e);
-	}
+var server = app.listen(8090, function () {
+    console.log("Server listening on port 8090");
 });
 
-app.use('/docs/api/node_modules',express.static(path.join(__dirname,'/node_modules')));
+/***************************************************
+ *              METRIC ROUTES
+ **************************************************/
 
-app.get('*',function (req,res){
-    res.redirect("/docs/api");
+/**
+ * This function starts a python process to perform
+ * the metric analysis specified. The following json
+ * structure will be returned:
+ *
+ * { "success": true,
+ *   "results": {output}
+ * }
+ *
+ */
+app.get('/api/doMetric/:metricName', function (req, res) {
+
+    // Get all the metrics
+    metricTypes = config.get('metricTypes');
+
+    // Find the metric requested
+    metric = metricTypes.find(function(m){
+        return m["alias"] === req.params.metricName;
+    });
+
+    // If there is no metric found then send res now.
+    if(metric === undefined){
+        res.send({"success":false,"msg":"no metric found"})
+    } else { // Else start the metric job
+        console.log("Running:", metric["alias"], " with filename:", metric["fileName"]);
+
+        // Get the location
+        loc = config.get('metricDir') + "/" + metric["fileName"];
+
+        // Spawn the child process to run the metric
+        const pyTest = spawn('python', [loc]);
+
+        body = '';
+
+        // put all of stdout into body
+        pyTest.stdout.on('data', function (data) {
+            body += data.toString()
+        });
+
+        // log any err's to console
+        pyTest.stderr.on('data', function (data) {
+            console.log(data.toString())
+        });
+
+        // when child process is done log the exit code
+        // and send the results to the browser
+        pyTest.on('close', function (code) {
+            console.log("Exit Code:", code.toString());
+
+            // Convert string body to json body
+            body = JSON.parse(body.split("'").join("\""));
+
+            // wrap the results in a json
+            result = {"sucess":true,"results": body};
+
+            // send the response
+            res.end(JSON.stringify(result));
+        });
+    }
 });
 
-var server = app.listen(8090,function(){
-	console.log("Server listening on port 8090");
+/*
+ * This route returns all the metrics located in
+ * the default config with the following json
+ * structure:
+ *
+ * { "success": true,
+ *   "results":[
+ *      {
+ *          "alias":"typeOne",
+ *          "fileName":"pyTest.py"
+*       },
+ *      {
+ *          "alias":"typeTwo",
+ *          "fileName":"pyTest2.py"
+*       }
+ *   ]
+ * }
+ */
+app.get('/api/getMetricTypes',function(req,res){
+
+    // Get the metrics
+    metricTypes = config.get('metricTypes');
+
+    if(metricTypes === undefined){
+        res.send({"success":true,"msg":"no metrics found"})
+    } else {
+
+        // make a return obj
+        metrics = {"results": []};
+
+        // put the metric types there
+        metrics["results"] = metricTypes;
+
+        // send the metrics
+        res.send(metrics)
+    }
 });
+
+/***************************************************
+ *              PCAP ROUTES
+ **************************************************/
+
+// TODO: Write the routes
+
+/***************************************************
+ *              API DOCS
+ **************************************************/
+
+app.use(express.static(path.join(__dirname + "/src/docs/static")));
+
+app.get('/docs/api', function (req, res) {
+    try {
+        console.log(path.join(__dirname + '/src/docs/api-docs'));
+        var html = template({title: 'Home'});
+        res.send(html);
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+app.use('/docs/api/node_modules', express.static(path.join(__dirname, '/node_modules')));
